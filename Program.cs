83eac9be7fc0c2f50c870053e5c9e4017b9a8906
Uddhav Dahatonde <dahatondeuddhava@gmail.com>
2022-09-19 13:55:23 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment17sept
+{
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Rectangle R1 = new Rectangle(2.5, 2);
+    //        R1.Area();
+    //        Console.WriteLine(R1);
+    //    }
+    //}
+}
---
+++ Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment17sept
+{
+    //abstract public class shape
+    //{
+    //    public abstract void Area();
+
+    //}
+    //public class Rectangle : shape
+    //{
+    //    private double length;
+    //    private double width;
+    //    private double result;
+    //    public Rectangle(double length,double width)
+    //    {
+    //       this.length = length;    
+    //       this.width = width; 
+    //    }
+    //    public override void Area()
+    //    {
+    //        result = 2 * (length + width);
+    //    }
+    //    public override string ToString()
+    //    {
+    //        return $"Area of Rectangle is->{result}";
+    //    }
+
+    //}
+}
